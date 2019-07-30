8e18b7e836d6e0eb8dbed33781572e9710c20fda
JakubCelovsky <celovsky.jakubmail.com>
2019-07-30 17:24:15 +0200

    fix ToDo URL

--- src/app/todo/services/todo.service.ts
+++ src/app/todo/services/todo.service.ts
@@ -6,6 +6,9 @@ import { HttpClient } from '@angular/common/http';
 import { catchError } from 'rxjs/operators';
 import { handleHttpError } from 'src/app/shared/helpers/api.helper';
 import { AuthService } from 'src/app/auth/service/auth.service';
+import { Store, select } from '@ngrx/store';
+import { getCurrentCompany } from 'src/app/company/state/company.selectors';
+import { CompanyItem } from 'src/app/company/models/company.model';
 
 @Injectable({
     providedIn: 'root'
@@ -14,47 +17,66 @@ export class TodoService {
 
     constructor(
         private authService: AuthService,
-        private http: HttpClient
+        private http: HttpClient,
+        private store: Store<any>
     ) { }
+
+    
+    currentCompanyId: number;
+    private companyId(): number {
+        this.store.pipe(select(getCurrentCompany)).subscribe(
+            (currentCompany: CompanyItem) => {
+              if(currentCompany != null) {
+                this.currentCompanyId = currentCompany.id;
+                return this.currentCompanyId;
+              }
+              else {
+                this.currentCompanyId = 0;
+                return this.currentCompanyId;
+              }
+            }
+          );  
+          return this.currentCompanyId;
+    }
 
     public getTodoList(): Observable<TodoListItem[]> {
         return this.http
-            .get<TodoItem[]>(this.createApiUrl('organizations/1/ToDos'))
+            .get<TodoItem[]>(this.createApiUrl(`organizations/${this.companyId()}/ToDos`))
             .pipe(
                 catchError(handleHttpError)
             );
     }
 
     public getTodo(id: number): Observable<TodoItem> {
-        return this.http.get(this.createApiUrl('ToDos', id.toString()))
+        return this.http.get(this.createApiUrl(`organizations/${this.companyId()}/ToDos`, id.toString()))
             .pipe(
                 catchError(handleHttpError)
             );
     }
 
     public addNewTodo(newTodo: NewTodoItem): Observable<any> {
-        return this.http.post(this.createApiUrl('ToDos'), newTodo)
+        return this.http.post(this.createApiUrl(`organizations/${this.companyId()}/ToDos`), newTodo)
             .pipe(
                 catchError(handleHttpError)
             );
     }
 
     public deleteCompletedTodo(): Observable<any> {
-        return this.http.delete(this.createApiUrl('ToDos', 'deleteCompleted'))
+        return this.http.delete(this.createApiUrl(`organizations/${this.companyId()}/ToDos`, 'deleteCompleted'))
             .pipe(
                 catchError(handleHttpError)
             );
     }
 
     public deleteTodo(id: number): Observable<any> {
-        return this.http.delete(this.createApiUrl('ToDos', id.toString()))
+        return this.http.delete(this.createApiUrl(`organizations/${this.companyId()}/ToDos`, id.toString()))
             .pipe(
                 catchError(handleHttpError)
             );
     }
 
     public updateTodo(updateTodo: UpdateTodoItem): Observable<any> {
-        return this.http.put(this.createApiUrl('ToDos', updateTodo.id.toString()), updateTodo)
+        return this.http.put(this.createApiUrl(`organizations/${this.companyId()}/ToDos`, updateTodo.id.toString()), updateTodo)
             .pipe(
                 catchError(handleHttpError)
             );
