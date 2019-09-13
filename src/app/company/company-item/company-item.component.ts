225185f8f0863876c5ab67d6e723a97c30a48e71
Martin Mikula <martin.mikula@kros.sk>
2019-09-13 10:37:50 +0200

    Highlighting tab buttons

--- src/app/company/company-item/company-item.component.ts
+++ src/app/company/company-item/company-item.component.ts
@@ -1,22 +1,18 @@
-import { Component, OnInit, Input } from "@angular/core";
-import { CompanyListComponent } from "../company-list/company-list.component";
-import { CompanyItem } from "../models/company.model";
-import { CompanyState } from "../state/company.state";
-import { Store } from "@ngrx/store";
-import * as companyActions from "../state/company.actions";
-import { NgbModal } from "@ng-bootstrap/ng-bootstrap";
-import { CompanyDetailComponent } from "../company-detail/company-detail.component";
-import { Router } from "@angular/router";
+import { Component, OnInit, Input } from '@angular/core';
+import { CompanyItem } from '../models/company.model';
+import { CompanyState } from '../state/company.state';
+import { Store } from '@ngrx/store';
+import * as companyActions from '../state/company.actions';
+import { Router } from '@angular/router';
 
 @Component({
-    selector: "kros-company-item",
-    templateUrl: "./company-item.component.html",
-    styleUrls: ["./company-item.component.scss"]
+    selector: 'kros-company-item',
+    templateUrl: './company-item.component.html',
+    styleUrls: ['./company-item.component.scss']
 })
 export class CompanyItemComponent implements OnInit {
     constructor(
         private store: Store<CompanyState>,
-        private modalService: NgbModal,
         private router: Router
     ) {}
 
@@ -28,15 +24,7 @@ export class CompanyItemComponent implements OnInit {
     }
 
     editCompany(id: number) {
-        // const modalRef = this.modalService.open(CompanyDetailComponent, {
-        //     size: "lg",
-        //     centered: true
-        // });
-        // modalRef.componentInstance.companyId = id;
-        console.log("nieco");
-        console.log(this.router.navigate(["settings", id]));
-        this.router.navigate(["settings/{id}"]);
-        // this.router.navigateByUrl("/settings/");
+        this.router.navigate(['settings', id]);
     }
     selectCompany(id: number) {
         this.store.dispatch(new companyActions.SetCurrentCompany(id));
